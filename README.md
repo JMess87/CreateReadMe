376446596d728efe104186f4506231fa137a2566
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-01-12 21:23:14 -0500

    pushing up code to start my own readme updates as well as json package, and index.js back end

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-
+# Jenny's Project 
+## Testing what I am doing 
+## Install npm
+## use as needed
+## MIT
+## no pushes
+## no requirements
+JMess87
+jennilee.messenger@gmail.com
---
+++ index.js
@@ -0,0 +1,80 @@
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is your project description?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Do you have an installation instructions?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is your usage information?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: [ "BSD", "MIT", "GPL"]
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are your contribution guidelines?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are your testing requirements?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Enter your email for additional questions',
+        },
+    ])
+
+    .then((answers) => {
+        const readmePageContent = generateReadme(answers);
+        fs.writeFile('README.md', readmePageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created ReadMe.md !')
+        );
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("You have an error!!!!!!!")
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            console.log("Blue Screen of Death !!!!")
+            // Something else went wrong
+        }
+    });
+
+const generateReadme = ({ title, description, installation, usage, license, contribution, test, github, questions}) =>
+`# ${title} 
+## ${description}
+## ${installation}
+## ${usage}
+## ${license}
+## ${contribution}
+## ${test}
+${github}
+${questions}
+`
